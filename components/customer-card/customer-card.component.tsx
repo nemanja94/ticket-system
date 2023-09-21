70bf6d44f32cd8c2044dc71b6f55528e7c40cd4e
nemanja94 <aandric.nemanja@gmail.com>
2023-09-21 12:55:48 +0200

    new font size in card component

--- components/customer-card/customer-card.component.tsx
+++ components/customer-card/customer-card.component.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const CustomerCard: FunctionComponent<Props> = ({customer: Customer}) => {
     return (
-        <Card className="overflow-hidden border-none">
+        <Card className="overflow-hidden border-none w-full">
             <CardHeader
                 className={`${Customer.customerType === CUSTOMER_TYPE.PremiumCustomer ? "bg-amber-300" : "bg-slate-300"} overflow-hidden`}>
                 <CardTitle>{Customer.customerFirstName} <br/> {Customer.customerLastName}</CardTitle>
@@ -20,7 +20,7 @@ const CustomerCard: FunctionComponent<Props> = ({customer: Customer}) => {
             </CardHeader>
             <CardContent className="bg-zinc-200">
                 <p className="pt-2 text-lg font-semibold">{Customer.customerType}</p>
-                <p className="pt-2 text-2xl font-semibold">{Customer.customerNumber}</p>
+                <p className="pt-2 text-xl font-semibold">{Customer.customerNumber}</p>
             </CardContent>
             {/*<CardFooter className="bg-zinc-300 flex justify-center items-center">*/}
             {/*    <p>-{Math.floor(Math.random() * 1000)}</p>*/}
