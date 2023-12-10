9aa70f05f140ff9d3017242e998ea2a13e52b0e4
Nemanja Andric <nemanja.andric.1994@outlook.com>
2023-12-10 08:31:05 +0100

    Updated stack

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,12 +4,19 @@ import { Firestore, getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAqcMVLZ2S553UImlzVW7Wl_IGZYduUmlk",
+
+  authDomain: "ticketsystem-294fd.firebaseapp.com",
+
+  projectId: "ticketsystem-294fd",
+
+  storageBucket: "ticketsystem-294fd.appspot.com",
+
+  messagingSenderId: "314560138780",
+
+  appId: "1:314560138780:web:c2fc66f9ab9df0d3c759d9",
+
+  measurementId: "G-E7Z30R1RWZ",
 };
 
 // Initialize Firebase
