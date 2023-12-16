fa8fdd6bd3d3341f5219bbee77d040f86d640796
Nemanja Andric <nemanja.andric.1994@outlook.com>
2023-12-16 21:18:43 +0100

    asd

--- src/app/customers/page.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./styles.module.scss";
-
-export default function Customers() {
-  return <div className={styles.container}></div>;
-}
--- src/app/vehicles/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Vehicles() {
-  return <div>Vehicles</div>;
-}
