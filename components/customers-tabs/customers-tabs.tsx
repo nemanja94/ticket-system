1a58bcda775859a379b8aa815a154b36633bdaf1
Nemanja Andric <aandric.nemanja@gmail.com>
2025-05-03 18:27:58 +0200

    Remove customer list tab and pagination component

--- components/customers-tabs/customers-tabs.tsx
+++ components/customers-tabs/customers-tabs.tsx
@@ -1,10 +1,8 @@
 import CustomerAddForm from "./customer-add-form";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import CustomerSearchForm from "./customer-search-form";
-import CustomersPaginated from "./customers-paginated";
 
 const enum TAB_TYPE {
-  GET_CUSTOMERS = "getCustomers",
   FIND_CUSTOMER = "findCustomer",
   ADD_CUSTOMER = "addCustomer",
 }
@@ -12,17 +10,11 @@ const enum TAB_TYPE {
 export default function CustomersTabs() {
   return (
     <Tabs
-      defaultValue={TAB_TYPE.GET_CUSTOMERS}
+      defaultValue={TAB_TYPE.FIND_CUSTOMER}
       className="flex w-[95%] flex-col"
     >
       {/* Tabs */}
       <TabsList className="bg-zinc-900/60">
-        <TabsTrigger
-          className="text-slate-200 w-full"
-          value={TAB_TYPE.GET_CUSTOMERS}
-        >
-          Pregled musterija
-        </TabsTrigger>
         <TabsTrigger
           className="text-slate-200 w-full"
           value={TAB_TYPE.FIND_CUSTOMER}
@@ -38,12 +30,6 @@ export default function CustomersTabs() {
       </TabsList>
 
       {/* Tabs content */}
-      <TabsContent
-        value="getCustomers"
-        className="flex flex-col justify-center items-start"
-      >
-        <CustomersPaginated />
-      </TabsContent>
       <TabsContent
         value="findCustomer"
         className="flex flex-col mt-6 justify-center items-center"
