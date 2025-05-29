5ddea1bbadab64f7763c6e87a784d7c62759cdf4
Nemanja Andric <aandric.nemanja@gmail.com>
2025-05-29 22:16:14 +0200

    Azurirani tipovi podataka.

--- Entities/Ticket.model.ts
+++ Entities/Ticket.model.ts
@@ -1,49 +1,49 @@
-import {Timestamp} from "firebase/firestore";
-import {Repairman} from "./Reapirman.model";
-import {Vehicle} from "./Vehicle.model";
+import { Timestamp } from "firebase/firestore";
+import { Repairman } from "./Reapirman.model";
+import { Vehicle } from "./Vehicle.model";
 
 export enum TicketPriority {
-    VisokPrioritet = "Visok prioritet",
-    SrednjiPrioritet = "Srednji prioritet",
-    NizakPrioritet = "Nizak prioritet",
+  VisokPrioritet = "Visok prioritet",
+  SrednjiPrioritet = "Srednji prioritet",
+  NizakPrioritet = "Nizak prioritet",
 }
 
 export class Ticket {
-    constructor(
-        ticketId: number,
-        repairmanId: Repairman["repairmanId"],
-        repairmanFirstName: Repairman["repairmanFirstName"],
-        repairmanLastName: Repairman["repairmanLastName"],
-        vehicleId: Vehicle["vehicleId"],
-        ticketDesc: string,
-        ticektPrice: number,
-        ticketPriority: TicketPriority,
-        ticketDateCreated: Timestamp | string,
-        ticketDateUpdated?: Timestamp,
-        ticketDateDeleted?: Timestamp
-    ) {
-        this.ticketId = ticketId;
-        this.repairmanId = repairmanId;
-        this.repairmanFirstName = repairmanFirstName;
-        this.repairmanLastName = repairmanLastName;
-        this.vehicleId = vehicleId;
-        this.ticketDesc = ticketDesc;
-        this.ticektPrice = ticektPrice;
-        this.ticketPriority = ticketPriority;
-        this.ticketDateCreated = ticketDateCreated;
-        this.ticketDateUpdated = ticketDateUpdated;
-        this.ticketDateDeleted = ticketDateDeleted;
-    }
+  constructor(
+    ticketId: string,
+    repairmanId: Repairman["repairmanId"],
+    repairmanFirstName: Repairman["repairmanFirstName"],
+    repairmanLastName: Repairman["repairmanLastName"],
+    vehicleId: Vehicle["vehicleId"],
+    ticketDesc: string,
+    ticektPrice: number,
+    ticketPriority: TicketPriority,
+    ticketDateCreated: Timestamp | string,
+    ticketDateUpdated?: Timestamp | string,
+    ticketDateDeleted?: Timestamp | string
+  ) {
+    this.ticketId = ticketId;
+    this.repairmanId = repairmanId;
+    this.repairmanFirstName = repairmanFirstName;
+    this.repairmanLastName = repairmanLastName;
+    this.vehicleId = vehicleId;
+    this.ticketDesc = ticketDesc;
+    this.ticektPrice = ticektPrice;
+    this.ticketPriority = ticketPriority;
+    this.ticketDateCreated = ticketDateCreated;
+    this.ticketDateUpdated = ticketDateUpdated;
+    this.ticketDateDeleted = ticketDateDeleted;
+  }
 
-    ticketId: number;
-    repairmanId: Repairman["repairmanId"];
-    repairmanFirstName: Repairman["repairmanFirstName"];
-    repairmanLastName: Repairman["repairmanLastName"];
-    vehicleId: Vehicle["vehicleId"];
-    ticketDesc: string;
-    ticektPrice: number;
-    ticketPriority: TicketPriority;
-    ticketDateCreated: Timestamp | string;
-    ticketDateUpdated?: Timestamp;
-    ticketDateDeleted?: Timestamp;
+  ticketId: string;
+  repairmanId: Repairman["repairmanId"];
+  repairmanFirstName: Repairman["repairmanFirstName"];
+  repairmanLastName: Repairman["repairmanLastName"];
+  vehicleId: Vehicle["vehicleId"];
+  ticketDesc: string;
+  ticektPrice: number;
+  ticketPriority: TicketPriority;
+  ticketDateCreated: Timestamp | string;
+  ticketDateUpdated?: Timestamp | string;
+  ticketDateDeleted?: Timestamp | string;
 }
